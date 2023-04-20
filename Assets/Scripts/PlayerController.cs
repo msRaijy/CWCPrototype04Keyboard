d725bb1f8fee54f1652fcf5a404e463b008dc107
Yvie Raij <gunghogirl@gmail.com>
2023-04-19 18:02:14 -0700

    CWCPrototype04.3
    New Functionality:
    When the player collects a powerup, a visual indicator appears
    When the player collides with an enemy while they have the powerup, the enemy goes flying
    After a certain amount of time, the powerup ability and indicator disappear

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRB;
     private GameObject focalPoint;
+    private float powerupStrength = 45.0f;
+
     public float speed = 5.0f;
+    public bool hasPowerup;
+    public GameObject powerupIndicator;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +24,38 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRB.AddForce(focalPoint.transform.forward * speed * forwardInput); 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            Debug.Log("We got the POWER!");
+            hasPowerup = true;
+            Debug.Log("The indicator is ON!");
+            powerupIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine() 
+    { 
+        yield return new WaitForSeconds(10); 
+        Debug.Log("We've lost power!");
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false); 
+    } 
+
+    private void OnCollisionEnter(Collision collision) 
+    { 
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup) 
+        { 
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>(); 
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log("Collided with " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse); 
+        }
     }
 }
